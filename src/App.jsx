307d4cb7a174b170c73f7c0bf9f07847cb860828
mike <josemimore0@gmail.com>
2024-07-02 08:10:16 -0400

    states with latitude

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,32 @@ import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "./App.css";
 import "../node_modules/leaflet/dist/leaflet.css";
+import states from "./states.json";
 
 function App() {
+    console.log(states);
     return (
         <>
-            <MapContainer center={[10.4806, -66.9036]} zoom={13}>
+            <MapContainer
+                center={[8.5238, -66.5897]}
+                zoom={6}
+                bounds={[6.4238, -66.5897]}
+                maxZoom={8}
+                minZoom={5}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                {states.map((state) => (
+                    <>
+                        {state.latitude && (
+                            <Marker position={state.latitude}>
+                                <Popup>{state.name}</Popup>
+                            </Marker>
+                        )}
+                    </>
+                ))}
             </MapContainer>
         </>
     );
